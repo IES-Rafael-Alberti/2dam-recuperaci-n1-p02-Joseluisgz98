0281fcc0a338e69798999a341b2e480756a4b14e
joseluisgz98 <joseluis_cadiz98@hotmail.com>
2024-02-16 12:18:54 +0100

    Clase enumerada Palos

---
+++ app/src/main/java/com/example/lacartamasalta/CardGames/data/Naipes.kt
@@ -0,0 +1,14 @@
+package com.example.lacartamasalta.CardGames.data
+
+enum class Naipes {
+    UNO,
+    DOS,
+    TRES,
+    CUATRO,
+    CINCO,
+    SEIS,
+    SIETE,
+    SOTA,
+    CABALLO,
+    REY
+}
--- app/src/main/java/com/example/lacartamasalta/CardGames/data/Palos.kt
+++ app/src/main/java/com/example/lacartamasalta/CardGames/data/Palos.kt
@@ -1,7 +1,6 @@
 package com.example.lacartamasalta.CardGames.data
 
 enum class Palos {
-    PORTADA,
     O,//OROS
     B,//BASTOS
     E,//ESPADAS
